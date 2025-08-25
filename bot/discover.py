e0f6e37f6094cf23b09c0cf612d3047ab07d3422
warmo1 <152406590+warmo1@users.noreply.github.com>
2025-08-25 08:46:58 +0100

    Update discover.py

--- bot/discover.py
+++ bot/discover.py
@@ -1,24 +1,19 @@
 from typing import List, Set
-from .exchange import get_exchange, get_uphold_assets
+from .exchange import get_data_exchange
 from .db import upsert_market, get_conn, init_schema
 from .config import cfg
 
 def discover_markets(database_url: str, quote: str | None = None) -> List[str]:
-    ex = get_exchange()
-    assets = get_uphold_assets(ex)
+    ex = get_data_exchange()
+    markets = ex.load_markets()
     conn = get_conn(database_url)
     init_schema(conn)
 
     discovered: List[str] = []
-
-    for asset in assets:
-        # Uphold provides assets, not pairs. We'll create pairs against our default quote currency.
-        base = asset.get('code')
-        q = quote or cfg.default_quote
-        symbol = f"{base}/{q}"
-        
-        # We don't have all the market data, so we'll make some assumptions
-        upsert_market(conn, (cfg.exchange, symbol, base, q, 1))
-        discovered.append(symbol)
-        
+    
+    for symbol, market in markets.items():
+        if quote and market.get('quote') == quote and market.get('active'):
+            upsert_market(conn, (cfg.data_source_exchange, symbol, market.get('base'), market.get('quote'), 1))
+            discovered.append(symbol)
+            
     return sorted(discovered)
